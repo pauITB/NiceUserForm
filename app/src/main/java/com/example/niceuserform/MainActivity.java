e5617dc9888df154c09b5f9e73b1e1d2644a412a
pauITB <pau.gisbert.7e3@itb.cat>
2021-01-25 17:32:49 +0100

    LogIn activity complette

--- app/src/main/java/com/example/niceuserform/MainActivity.java
+++ app/src/main/java/com/example/niceuserform/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.niceuserform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    MaterialButton logIn, register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.title_tect_view);
+        logIn = findViewById(R.id.logIn_button);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LogInActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
